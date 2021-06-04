08b3e948214b09a208290a0e22e10c4c76e0a714
Graner007 <graner.bence007@gmail.com>
2021-06-04 17:29:36 +0200

    implement location entity

--- src/main/java/com/codecool/report/model/Location.java
+++ src/main/java/com/codecool/report/model/Location.java
@@ -1,4 +1,35 @@
 package com.codecool.report.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.UUID;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+@Builder
+@Entity
 public class Location {
+
+    @Id
+    private UUID id;
+
+    private String managerName;
+
+    private String phone;
+
+    private String addressPrimary;
+
+    private String addressSecondary;
+
+    private String country;
+
+    private String town;
+
+    private String postalCode;
 }
