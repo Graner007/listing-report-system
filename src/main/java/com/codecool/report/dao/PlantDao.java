2c28b93cbfdae78939c4e6982f4e57fa3f46d349
Graner007 <graner.bence007@gmail.com>
2021-06-04 19:13:30 +0200

    add locationDao, statusDao and marketplaceDao with basic insert, get and delete methods

--- src/main/java/com/codecool/report/dao/PlantDao.java
+++ src/main/java/com/codecool/report/dao/PlantDao.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface PlantDao {
 
-    void add(Plant artist);
+    void add(Plant plant);
     Plant find(int id);
     void remove(int id);
     void removeAll();
